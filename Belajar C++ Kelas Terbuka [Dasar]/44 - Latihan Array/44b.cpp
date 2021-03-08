e005383e6eaf9b567f3f5b3bd71b48c2cc86db90
Irwandi Refraugati <65169347+irwandirefraugati@users.noreply.github.com>
2021-03-08 07:13:50 +0700

    Create 44b.cpp

---
+++ Belajar C++ Kelas Terbuka [Dasar]/44 - Latihan Array/44b.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+	array<int, 10> nilai;
+
+	cout << "Program menampilkan grafik nilai " << endl << endl;
+
+	for (int i = 0; i <= nilai.size(); i++) {
+		cout << "jumlah mahasiswa dengan nilai ";
+		if (i == 0) {
+			cout << "0-9: ";
+		}
+		else if (i == 10) {  //yg membuat limit inputan
+			cout << "100: ";
+		}
+		else{
+			cout << i * 10 << "-" << (i * 10) + 9 << ": ";
+		}	
+		cin >> nilai[i];
+	}
+	for (int i = 0; i <= nilai.size(); i++) {
+		cout << "jumlah mahasiswa dengan nilai ";
+		if (i == 0) {
+			cout << "0-9: ";
+		}
+		else if (i == 10) {  //yg membuat limit inputan
+			cout << "100: ";
+		}
+		else {
+			cout << i * 10 << "-" << (i * 10) + 9 << ": ";
+		}
+	}
+	cin.get();
+	return 0;
+}
